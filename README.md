d24818212bccd04fe2630151104e43bd20b025c9
ciscoo <cisco@mateo.io>
2015-08-04 20:35:23 -0500

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Thuy
+## Overview
+
+The source for little notes to my girlfriend on the Internet. The notes are private and will not be uploaded, but everything else will. Built with [Sass](http://sass-lang.com/), [Gulp](http://gulpjs.com/), [Bower](http://bower.io/),
+[Susy](http://susy.oddbird.net/), and [Jekyll](jekyllrb.com)
+
+## Quickstart
+
+Ensure you have [Gulp](http://gulpjs.com/) installed globally with `$ npm install -g gulp bower-cli`
+
+1. Clone the repo: `$ git clone https://github.com/ciscoo/thuy.git`
+2. Run `$ npm install` to install all the `devDependencies`
+3. Then `$ bower install` to install Susy.
+4. Finally run `$ gulp` to launch the development server.
+
+## License
+
+MIT  
+Copyright 2015 Francisco mateo
---
+++ gulpfile.babel.js
@@ -0,0 +1,53 @@
+import gulp from 'gulp';
+import cp from 'child_process';
+import browserSync from 'browser-sync';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import pkg from './package.json';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+gulp.task('jekyll:build', done => {
+  return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+gulp.task('jekyll:rebuild', ['jekyll:build'], () => {
+  reload;
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/img/**')
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'images'}));
+});
+
+gulp.task('styles', () => {
+  const AUTOPREFIXER_BROWSERS = ['last 2 versions, > 5%'];
+
+  return gulp.src(['styles/**/*.css'])
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      includePaths: ['bower_components/susy/sass'],
+      precision: 10
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('_site'))
+    .pipe($.size({title: '_site'}));
+});
+
+gulp.task('serve', ['styles', 'jekyll:build', 'images'], () => {
+  const DIRS = ['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*.html'];
+
+  browserSync({
+    server: '_site'
+  });
+
+  gulp.watch(['styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(DIRS, ['jekyll:rebuild']);
+  //gulp.watch(['dist/scripts/**/*.js'], ['jshint']);
+  //gulp.watch(['dist/images/**/*'], reload);
+});
+
+gulp.task('default', ['serve']);
